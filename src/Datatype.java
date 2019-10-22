44af3cc3f2a140bdb3c25c44ad44649f1ad0deb0
GBTC407003UR <GBTC407003UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-22 10:19:56 -0400

    firstcommit

---
+++ src/Datatype.java
@@ -0,0 +1,2 @@
+Write an application that assigns 2 to a variable named mynumber and 1.7938 to a variable namedmyothernumber, and then prints out the values to the console.
+
---
+++ src/Math2.java
@@ -0,0 +1,9 @@
+public class Math2 {
+    public static void main(String[] args) {
+        int mynumber =2;
+        double myothernumber = 1.7938;
+        System.out.println("mynum is" + mynumber);
+                System.out.println("the value of myothernumber is " + myothernumber);
+
+    }
+}
